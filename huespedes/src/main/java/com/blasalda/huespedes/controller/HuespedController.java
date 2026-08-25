8e4fc629f3be8134853f786a85ea52c78ac00ed3
AldahirM <l21te0275@teziutlan.tecnm.mx>
2026-08-25 11:53:32 -0600

    Endpoint de obtención de huéspedes sin importar estado terminado

--- huespedes/src/main/java/com/blasalda/huespedes/controller/HuespedController.java
+++ huespedes/src/main/java/com/blasalda/huespedes/controller/HuespedController.java
@@ -4,11 +4,20 @@ import com.blasalda.commons.controller.CommonController;
 import com.blasalda.commons.dto.huespedes.HuespedRequest;
 import com.blasalda.commons.dto.huespedes.HuespedResponse;
 import com.blasalda.huespedes.service.HuespedService;
+import jakarta.validation.constraints.Positive;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HuespedController extends CommonController<HuespedRequest, HuespedResponse, HuespedService> {
     public HuespedController(HuespedService service) {
         super(service);
+    }
+
+    @GetMapping("/id-huesped/{id}")
+    public ResponseEntity<HuespedResponse> obtenerPorIdSinEstado(@Positive(message = "El id debe ser positivo") @PathVariable Long id) {
+        return ResponseEntity.ok(service.obtenerPorIdSinEstado(id));
     }
 }
