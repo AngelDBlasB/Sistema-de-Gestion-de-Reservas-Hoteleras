8222ac697a5c52925a8796e2f1f6bb0067c3c1e2
AldahirM <l21te0275@teziutlan.tecnm.mx>
2026-08-24 20:14:08 -0600

    Módulo de huéspedes con primeras validaciones

---
+++ huespedes/src/main/java/com/blasalda/huespedes/service/HuespedService.java
@@ -0,0 +1,11 @@
+package com.blasalda.huespedes.service;
+
+import com.blasalda.commons.dto.huespedes.HuespedRequest;
+import com.blasalda.commons.dto.huespedes.HuespedResponse;
+import com.blasalda.commons.service.CrudService;
+import com.blasalda.huespedes.entity.Huesped;
+
+public interface HuespedService extends CrudService<HuespedRequest, HuespedResponse> {
+
+
+}
